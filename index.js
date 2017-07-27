b8538653f14401f1fef7aeddfa92e0d1fc9a412e
Michael Harrington <mike85h@gmail.com>
2017-07-27 15:19:21 -0600

    fml2

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-$.get("https://frozen-ridge-41004.herokuapp.com/", function(data){
-  console.log(data[0]);
-  console.log(data[1]);
-  console.log(data[2]);
-  console.log(data[3]);
-  console.log(data[4]);
-})
+
+
+
+$( document ).ready(function() {
+  $.get("https://frozen-ridge-41004.herokuapp.com/", function(data){
+    console.log(data[0]);
+    console.log(data[1]);
+    console.log(data[2]);
+    console.log(data[3]);
+    console.log(data[4]);
+});
