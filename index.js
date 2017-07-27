a1881fe90b9fdb1ff545cfa2c9de4afe1dbe9dfe
Michael Harrington <mike85h@gmail.com>
2017-07-27 15:02:22 -0600

    Finally working with remote db. JSON is being served to client

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log('working');
-
-$.get( "http://localhost:8080", function( data ) {
-  console.log(data);
-});
+$.get("https://frozen-ridge-41004.herokuapp.com/", function(data){
+  console.log(data[0]);
+  console.log(data[1]);
+  console.log(data[2]);
+  console.log(data[3]);
+  console.log(data[4]);
+})
