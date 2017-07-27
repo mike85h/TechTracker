58ab29131b43d1c8c3b255c0b0cfd1e1e500032c
Michael Harrington <mike85h@gmail.com>
2017-07-27 10:52:57 -0600

    farthest until sqlite3 error

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 console.log('working');
+
+// const url = 'http://localhost:8080/'
+console.log("working");
+
+// $.get( "localhost:8080/", function( data ) {
+//   console.log(data);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// getInfo(url)
+//
+// function getInfo(url){
+//   $.get(url + 'all').then(data => data.json())
+//     .then(displayInfo)
+// }
+//
+// function displayInfo(info){
+//   info.foEach(info => {
+//     const li = document.createElement('li')
+//     const ul = document.querySelector('ul')
+//     li.innerHTML = `Id: ${info.id}, Name: ${info.name}`
+//     ul.append(li)
+//   })
+// }
